9523f2ffb89294876c5acc0a8f631bec45861097
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-04-16 17:32:07 +0300

    Update server.py
    added functions for server

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import random
+import hashlib
 
 class Server:
 
@@ -14,38 +16,48 @@ class Server:
         self.s.bind((self.host, self.port))
         self.s.listen(100)
 
-        # generate keys ...
+        self.public_key, self.private_key = generate_keypair(61, 53)
 
         while True:
             c, addr = self.s.accept()
             username = c.recv(1024).decode()
             print(f"{username} tries to connect")
-            self.broadcast(f'new person has joined: {username}')
+           
             self.username_lookup[c] = username
             self.clients.append(c)
 
-            # send public key to the client 
+            # send public key to the client
+            c.send(str(self.public_key).encode())
 
-            # ...
+            # receive client's public key
+            client_pk = eval(c.recv(1024).decode())
+            self.client_keys[c] = client_pk
 
             # encrypt the secret with the clients public key
+            secret = random.randint(1, 255)
+            self.session_keys[c] = secret
 
-            # ...
+            e, n = client_pk
+            encrypted_secret = pow(secret, e, n)
 
-            # send the encrypted secret to a client 
-
-            # ...
-
+            # send the encrypted secret to a client
+            c.send(str(encrypted_secret).encode())
+            self.broadcast(f'new person has joined: {username}')
             threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
+    
     def broadcast(self, msg: str):
-        for client in self.clients: 
+    
+        for client in self.clients:
+            msg_hash = hashlib.sha256(msg.encode()).hexdigest()
+    
+            client_pk = self.client_keys[client]
+            encrypted_msg = encrypt(client_pk, msg)
+    
+            packet = f"{msg_hash}::{encrypted_msg}"
+    
+            client.send(packet.encode())
 
-            # encrypt the message
-
-            # ...
-
-            client.send(msg.encode())
 
     def handle_client(self, c: socket, addr): 
         while True:
